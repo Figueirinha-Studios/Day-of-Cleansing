70c37fe0136625115eb8632fbc49ead684ea4a07
Vinícius Anjos de Matos <vinicius.am11@aluno.ifsc.edu.br>
2026-08-27 22:30:05 -0300

    Recepção serial para LCD e buzzer

--- hw/PICO2/main.py
+++ hw/PICO2/main.py
@@ -1,7 +1,8 @@
-import machine, time
+import machine, time, sys, uselect
 from lcd_api import LcdApi
 from pico_i2c_lcd import I2cLcd
 
+buzzer = machine.PWM(machine.Pin(28))
 sda=machine.Pin(26)
 scl=machine.Pin(27)
 i2c=machine.SoftI2C(sda=sda, scl=scl, freq=100000)
@@ -10,11 +11,35 @@ print(i2c.scan())
 lcd = I2cLcd(i2c, 0x27, 2, 16)
 lcd.putstr("Text goes here!")
 
+poll = uselect.poll()
+poll.register(sys.stdin, uselect.POLLIN)
 
-a = 0
+def tocar_nota(freq, duracao):
+    buzzer.freq(freq)
+    buzzer.duty_u16(32768)
+    time.sleep(duracao)
+    buzzer.duty_u16(0)
+
+buffer = ""
 
 while True:
-    lcd.clear()
-    lcd.putstr(str(a))
-    a = a + 1
-    time.sleep(1)
+    if poll.poll(0):
+        char = sys.stdin.read(1)
+
+        if char:
+            if char == "\n":
+                mensagem = buffer.strip()
+                buffer = ""
+                
+                if mensagem == 'limpa':
+                    lcd.clear()
+                elif mensagem:
+                    print("RECEBI:", repr(mensagem))
+                    lcd.clear()
+                    lcd.putstr(mensagem)
+                
+                
+            else:
+                buffer += char
+
+    time.sleep(0.01)
